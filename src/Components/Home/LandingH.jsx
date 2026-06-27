468ed4bf602a3b27c8a61eeb260f44fd28d80f7a
alihumdard-dev <alihumdard125@gmail.com>
2026-06-27 11:46:34 +0500

    renaming the files

--- src/Components/Home/LandingH.jsx
+++ src/Components/Home/LandingH.jsx
@@ -1,23 +1,18 @@
 import React from "react";
 import Hero from "./Hero";
-import Section2 from "./Section2";
-import Section3 from "./Section3";
-import Section4 from "./Section4";
-import Section5 from "./Section5";
-
-// Agar aap wo Pricing/Why Choose Us wala Dashboard bhi Home pe dikhana chahte hain to neeche wali line uncomment karein:
-// import Dashboard from "../Dashboard";
+import WhatIsGothm from "./WhatIsGothm";
+import EpisodesAndBlog from "./EpisodesAndBlog";
+import AboutHost from "./AboutHost";
+import GhostMailContact from "./GhostMailContact";
 
 const LandingH = () => {
   return (
     <>
       <Hero />
-      <Section2 />
-      <Section3 />
-      <Section4 />
-      <Section5 />
-
-      {/* <Dashboard /> */}
+      <WhatIsGothm />
+      <EpisodesAndBlog />
+      <AboutHost />
+      <GhostMailContact />
     </>
   );
 };
--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -3,12 +3,10 @@ import Navbar from "./Navbar";
 import Footer from "./Footer";
 
 export default function Layout() {
-  // Sidebar se related saari state aur useEffect yahan se hata di gayi hai.
 
   return (
     <div className="flex h-screen w-full overflow-hidden transition-colors duration-300 selection:bg-blue-500/30">
       <div className="relative flex h-full flex-1 flex-col overflow-hidden">
-        {/* Navbar se isSidebarOpen aur toggleSidebar props hata diye hain */}
         <Navbar />
 
         {/* Content Area */}
